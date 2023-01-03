e965c20ce45f96fe3a7cb0d9851ea367371e2432
jishna999 <95485984+jishna999@users.noreply.github.com>
2023-01-03 15:23:41 +0530

    Create MainActivity.java

---
+++ Program6/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+ ListView ls;
+ String[] list = {"Apple","Orange","Mango","Pineapple"};
+ @SuppressLint("MissingInflatedId")
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.activity_main);
+ ls = findViewById(R.id.ls);
+ ArrayAdapter<String> arr = new ArrayAdapter<String>(this, 
+android.R.layout.simple_spinner_item,list);
+try {
+ ls.setAdapter(arr);
+ }
+ catch (Exception e)
+ {
+ Toast.makeText(this, "Something went wrong", 
+Toast.LENGTH_SHORT).show();
+ }
+ }
+}
